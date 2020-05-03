38ddeccc9e4819f6a3abfb7e7a1f2aa89c782ad9
tomysnatalia <hanf.natalia@gmail.com>
2020-05-03 11:41:39 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
        $(document).ready(function() {
 
   var apiRoot = 'https://lit-dusk-81361.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://api.trello.com/1';
+  const trelloApiRoot = 'https://api.trello.com/1/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
